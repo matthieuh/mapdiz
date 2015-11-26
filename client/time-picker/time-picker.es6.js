07c498344ffe48c46c90c2010d71a72af4626239
unknown <matthieu.hostache@gmail.com>
2015-11-26 21:03:39 +0100

    Finish time-picker

--- client/time-picker/time-picker.es6.js
+++ client/time-picker/time-picker.es6.js
@@ -26,12 +26,23 @@ class TimePicker {
 
     self.hours = [];
     self.minutes = [];
+    self.answer = answer;
 
-    for(var h = 0; h < 24 ; h++) {
+    for(var i = 0; i < 24 ; i++) {
+      var h = ("0" + i).slice(-2);
       self.hours.push(h);
-      for(var m = 0; m < 12 ; m++) {
+      
+      if (i%2 == 0) {
+        var m = ("0" + i/2*5).slice(-2);
         self.minutes.push(m);
       }
+    }
+
+    ////////////
+
+    function answer() {
+      $scope.showDialog = false;
+      self.ngModel = moment().hour(self.timeHour).minutes(self.timeMinute).toDate();
     }
 
   }
