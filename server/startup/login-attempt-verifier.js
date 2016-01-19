7b0e3a2d9000da990e8386bbccd6d6a0174558e9
Matthieu HOSTACHE <matthieu.hostache@gp-aviva.com>
2016-01-19 12:16:32 +0100

    Add css slideshow on homepage

--- server/startup/login-attempt-verifier.js
+++ server/startup/login-attempt-verifier.js
@@ -4,9 +4,9 @@ Meteor.startup(function() {
       if (parameters.user && parameters.user.emails && (parameters.user.emails.length > 0)) {
         // return true if verified email, false otherwise.
         var found = _.find(
-                           parameters.user.emails, 
-                           function(thisEmail) { return thisEmail.verified }
-                          );
+          parameters.user.emails,
+          function(thisEmail) { return thisEmail.verified }
+        );
 
         if (!found) {
           throw new Meteor.Error(500, 'We sent you an email.');
